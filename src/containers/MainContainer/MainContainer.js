356eb1dfe0f44b98a04b7ec419b0fd46b81fcf72
mkubibaev <mkubibaev@gmail.com>
2019-01-24 15:06:06 +0600

    added main container

---
+++ src/containers/MainContainer/MainContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+export default MainContainer;
